e57846efd965c1dfcc1fce74eaa1211cac4e3174
Denis Nurmukhametov <denis.nurmukhametov@demicon.de>
2023-12-07 20:28:21 +0100

    added static with content

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,34 @@
 $(document).ready(function () {
-  let bg = document.querySelectorAll('.mouse-parallax-bg');
+  let parallaxBg = document.querySelectorAll('.mouse-parallax-bg');
 
-  for (let i = 0; i < bg.length; i++){
-    window.addEventListener('mousemove', function(e) {
+  for (let i = 0; i < parallaxBg.length; i++) {
+    window.addEventListener('mousemove', function (e) {
       let x = e.clientX / window.innerWidth;
       let y = e.clientY / window.innerHeight;
-      bg[i].style.transform = 'translate(-' + x * 40 + 'px, -' + y * 40 + 'px)';
+      parallaxBg[i].style.transform = 'translate(-' + x * 40 + 'px, -' + y * 40 + 'px)';
     });
   }
+
+  if ($('main').hasClass('hero-page')) {
+    $('.page-header').addClass('big-logo');
+  } else {
+
+  }
+
+  if ($(document).scrollTop() > 50) {
+    $('.page-header').removeClass('big-logo')
+  }
+
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 30) {
+      $('.page-header').removeClass('big-logo')
+      $('.page-header').addClass('scrolling');
+    } else {
+
+      if ($('main').hasClass('hero-page')) {
+        $('.page-header').addClass('big-logo');
+      }
+      $('.page-header').removeClass('scrolling');
+    }
+  });
 });
